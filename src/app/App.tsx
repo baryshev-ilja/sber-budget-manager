278047b94e322871189aed23fea0ed5040896eea
Baryshewww <baryshev-tm@mail.ru>
2024-11-12 14:38:41 +0300

    test(response): протестировать как работает адаптивность

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,75 @@
+import { Card } from '@/shared/ui/Card';
+import { Input } from '@/shared/ui/Input';
+import { Select } from '@/shared/ui/Select';
+import { VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text';
+import { Navbar } from '@/widgets/navbar';
+
+const items = [
+    {
+        value: 'A',
+        content: 'Доходы',
+    },
+    {
+        value: 'B',
+        content: 'Расходы',
+    },
+    {
+        value: 'D',
+        content: 'Расходы',
+    },
+    {
+        value: 'C',
+        content: 'Расходы',
+    },
+    {
+        value: 'E',
+        content: 'Расходы',
+    },
+];
+
 export const App = () => {
     return (
-        <div style={{ flexDirection: 'column' }} className="container">
-            <p className="display">Мой баланс:</p>
-            <p className="medium">23 120 000 ₽</p>
-            <p className="regular">Новая категория</p>
+        <div className="app">
+            <Navbar />
+            <div className="container" style={{ flexDirection: 'column' }}>
+                <Text as="h1" size="xxl" color="heading">
+                    Мой баланс:
+                </Text>
+                <Text size="big" color="green">23 120 000 ₽</Text>
+                <Text as="h2" size="xl" color="heading">Новая категория</Text>
+                <Card>
+                    <VStack>
+                        <Text size="xs" color="gray">Новая категория xs</Text>
+                        <Text size="s" color="gray">Новая категория s</Text>
+                        <Text size="m" color="grayAccent">Новая категория m</Text>
+                        <Text size="mBold" color="gray">Новая категория mBold</Text>
+                        <Text size="l" color="grayAccent">Новая категория l</Text>
+                        <Input
+                            label="Тип"
+                            placeholder="fsdfsadfasdf"
+                        />
+                        <Select
+                            onChange={() => {}}
+                            label="Категория"
+                            items={items}
+                            currentValue="A"
+                            variant="inversion"
+                        />
+                    </VStack>
+                </Card>
+                {/* <Input */}
+                {/*    label="Название" */}
+                {/*    placeholder="fsdfsadfasdf" */}
+                {/* /> */}
+                <Select
+                    onChange={() => {}}
+                    label="Категория"
+                    items={items}
+                    currentValue="A"
+                    direction="top"
+                />
+            </div>
         </div>
     );
 };
